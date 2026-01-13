4c01bbf81ce6907eca8e3e2952d59ec134f169dc
Franklyn <franklynahilon@gmail.com>
2026-01-13 11:42:45 -0800

    Initial Commit

---
+++ WordGarden/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  WordGarden
+//
+//  Created by AHILON CANO, FRANKLYN on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT", "DOG", "CAT"] // All Cas
+    @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
+    @State private var currentWord = 0
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    @State private var playAgainHidden = true
+    
+    var body: some View {
+     VStack {
+        HStack {
+            VStack (alignment: .leading) {
+                Text ("Words Guessed: \(wordsGuessed)")
+                Text("Words Missed: \(wordsMissed)")
+            }
+            Spacer()
+            VStack (alignment: .trailing) {
+                Text("Words to Guess \(wordsToGuess.count - (wordsGuessed + wordsMissed))")
+                Text("Words in Game: \(wordsToGuess.count)")
+            }
+        }
+        .padding(.horizontal)
+         
+        Spacer()
+         
+         Text(gameStatusMessage)
+             .font(.title)
+             .multilineTextAlignment(.center)
+             .padding()
+         
+         Text("________")
+             .font(.title)
+         
+         if playAgainHidden{
+             HStack {
+                 TextField("", text: $guessedLetter)
+                     .textFieldStyle(.roundedBorder)
+                     .frame(width: 30)
+                     .overlay {
+                         RoundedRectangle(cornerRadius: 5).stroke(.gray,lineWidth: 2)
+                     }
+                 Button("Guess a Letter:"){
+                 }
+                 .buttonStyle(.bordered)
+                 .tint(.mint)
+             }
+         } else {
+        Button("Another Word?"){
+         }
+         .buttonStyle(.borderedProminent)
+         .tint(.mint)
+         }
+         Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
